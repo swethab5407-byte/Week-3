a54361a995813b9fb0e38e8c5687c92de0bdbb96
Swetha <swethab5407@gmail.com>
2026-08-28 23:35:24 +0530

    Added voting eligibility program

---
+++ VotingEligibility.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        boolean eligible = age >= 18;
+
+        if (eligible) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
